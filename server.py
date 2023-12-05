3e0999d5567a481a40842f89e5f2faefc4230d83
Zak Laughton <me@zaklaughton.dev>
2023-12-05 09:03:02 -0600

    added initial files for python sync

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, redirect
 import csv
 
 app = Flask(__name__)
+print(__name__)
 
 @app.route('/')
 def my_home():
@@ -12,32 +13,39 @@ def html_page(page_name):
     return render_template(page_name)
 
 def write_to_file(data):
-  with open('database.txt', mode='a') as database:
-    email = data["email"]
-    subject = data["subject"]
-    message = data["message"]
-    file = database.write(f'\n{email},{subject},{message}')
+    # Writes to database.txt
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
 
 def write_to_csv(data):
-  with open('database.csv', newline='', mode='a') as database2:
-    email = data["email"]
-    subject = data["subject"]
-    message = data["message"]
-    csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    csv_writer.writerow([email,subject,message])
+    # Writes to database.csv
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
 
-
-@app.route('/submit_form', methods=['POST', 'GET'])
+@app.route('/submit_form', methods={'POST', 'GET'})
 def submit_form():
     if request.method == 'POST':
-      try:
-        data = request.form.to_dict()
-        write_to_csv(data)
-        return redirect('/thankyou.html')
-      except:
-        return 'did not save to database'
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
     else:
-      return 'something went wrong. Try again!'
+        "Something went wrong. Try again"
 
 
+# @app.route('/favicon.ico')
+# def blog():
+#     return 'These are my thoughts on blogs'
 
+@app.route('/blog/2020/dogs')
+def dog_blog():
+    return 'this is my dog'
